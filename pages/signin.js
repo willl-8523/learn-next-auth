f847dc860eaad5fcdce9c89e3c9794f0732ad509
willl85 <ngowilfried@gmail.com>
2024-03-12 12:22:03 +0100

    Add sign in form with credential and signInUser feature

--- pages/signin.js
+++ pages/signin.js
@@ -4,8 +4,31 @@ import {
   getSession,
   signIn,
 } from 'next-auth/react';
+import Router, { useRouter } from 'next/router';
+import { useState } from 'react';
 
 export default function SignIn({ csrfToken, providers }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState(null);
+
+  const signInUser = async (e) => {
+    e.preventDefault();
+    let options = { redirect: false, email, password };
+    const res = await signIn('credentials', options);
+    setMessage(null);
+
+    if (res?.error) {
+      setMessage(res.error);
+      // return;
+    }
+
+    console.log(res);
+    console.log(email, password);
+
+    // return Router.push('/');
+  };
+
   return (
     <>
       <form method="post" action="/api/auth/signin/email">
@@ -16,8 +39,36 @@ export default function SignIn({ csrfToken, providers }) {
         </label>
         <button type="submit">Sign in with Email</button>
       </form>
+
+      <form action="">
+        <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+        <label>
+          Email
+          <input
+            type="email"
+            id="email-credential"
+            name="email-credential"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <p style={{ color: 'red' }}>{message}</p>
+        <button onClick={(e) => signInUser(e)}>Sign in with Credentials</button>
+        <button>Sign up</button>
+      </form>
+
       {Object.values(providers).map((provider) => {
-        if (providers.name === 'email') {
+        if (provider.name === 'Email' || provider.name === 'Credentials') {
           return;
         }
         return (
